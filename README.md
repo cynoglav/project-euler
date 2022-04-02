0e0ada4a81ddf4b26852bc09b7e9822c3965b031
cyno <63934199+cynoglav@users.noreply.github.com>
2022-04-02 11:45:44 +0200

    starting work on sum of multiples
    - Updated README
    - Added boilerplate doc
    - Wrote down method for solving SumOfMultiples
    
    Todos:
    - Implement SumOfMultiples
    
    Notes:
    - I guess starting at 1000 and decrementing also works?

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 ## project-euler
+
 Trying to solve projecteuler.net problems!
-
-
 
 ### currently working on:
 
-nothing yet! learning git first :)
+multiples of 3 and 5 (1/x)
 
 ### next up:
-multiples of 3 and 5 (1/x)
+
+even fibonacci numbers (2/x)
---
+++ work-in-progress/SumOfMultiples.java
@@ -0,0 +1,26 @@
+
+public class SumOfMultiples {
+    public static void main(String[] args) {
+
+        int checkNum = 3;
+        int Sum = 0;
+
+        /*
+         * If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+         * get 3, 5, 6 and 9. The sum of these multiples is 23.
+         * 
+         * Find the sum of all the multiples of 3 or 5 below 1000.
+         */
+
+        /*
+         * how-to:
+         * start at n=3, bc that's the smallest one that matters,
+         * increment by one and check divisibility:
+         * if n%3 OR n%5 = 0, then add n to SUM
+         * once n=1000, perform last check then print SUM
+         * 
+         * is there a more elegant way of doing this?
+         * if there is, then I don't know it :P
+         */
+    }
+}
